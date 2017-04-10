31a90572044f18409265dab02f05c3c0cad812d8
Jannes Höke <mail@jannes-hoeke.de>
2017-04-10 23:00:08 +0200

    switch to a json feed

--- feed.py
+++ feed.py
@@ -1,5 +1,12 @@
-from flask import Flask
+import json
+from datetime import datetime
+import locale
+
+from flask import Flask, Response
 import requests
+from bs4 import BeautifulSoup
+
+locale.setlocale(locale.LC_TIME, "C")
 
 app = Flask(__name__)
 
@@ -8,15 +15,40 @@ app = Flask(__name__)
 # def einslive():
 #    requests.get('')
 
+SINGLE_AUDIO = {
+  "uid": None,
+  "updateDate": None,
+  "titleText": None,
+  "mainText": "",
+  "streamUrl": None,
+  "redirectionUrl": None
+}
+
+PODCAST_URL = 'https://www1.wdr.de/mediathek/audio/%s/%s.podcast'
+HTML_URL = 'https://www1.wdr.de/mediathek/audio/%s/%s.html'
+
 feeds = {
-    'wdr-aktuell': 'https://www1.wdr.de/mediathek/audio/wdr-aktuell/wdr-aktuell-1-100.podcast',
-    'wdr-zeitzeichen': 'http://www1.wdr.de/mediathek/audio/zeitzeichen/zeitzeichen-1-100.podcast',
+    'wdr-aktuell': ('wdr-aktuell', 'wdr-aktuell-1-100'),
+    'wdr-zeitzeichen': ('zeitzeichen', 'zeitzeichen-1-100'),
 }
 
 @app.route("/<feed>")
 def get_feed(feed):
-    r = requests.get(feeds[feed])
-    return r.text.replace('http://podcast-ww.wdr.de', 'https://media.data.wdr.de')
+    r = requests.get(PODCAST_URL % feeds[feed])
+    soup = BeautifulSoup(r.text, 'lxml')
+    resp = SINGLE_AUDIO.copy()
+    chan = soup.rss.channel
+
+    resp['uid'] = chan.guid.string
+    # Mon, 10 Apr 2017 16:00:16 GMT
+    update_time = datetime.strptime(chan.lastbuilddate.string, '%a, %d %b %Y %H:%M:%S %Z')
+    resp['updateDate'] = update_time.strftime('%Y-%m-%dT%H:%M:%S.0Z')  # 2017-04-10T16:00:16.0Z
+    resp['titleText'] = chan.title.string
+    resp['streamUrl'] = chan.item.enclosure['url'].replace('http://podcast-ww.wdr.de', 
+                                                           'https://media.data.wdr.de')
+    resp['redirectionUrl'] = HTML_URL % feeds[feed]
+    
+    return Response(json.dumps(resp), mimetype='application/json')
 
 if __name__ == "__main__":
     app.run()
